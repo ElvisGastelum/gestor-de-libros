6a09c62887b2e70066fb8c28646e4e9cadb75736
Elvis Gastelum <elvisgastelum@outlook.com>
2019-05-19 14:04:05 -0700

    Commit Inicial
    Gestor de libros - primer commit

---
+++ Gestor_De_Libros/Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.IO;
+using System.Security.AccessControl;
+
+namespace Gestor_De_Libros
+{
+	
+	class Program
+	{
+		readonly public string pathDataProject = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\dataProject";
+
+		public string PathDataProject {
+			get {
+				return pathDataProject;
+			}
+			
+		}
+		public static void Main(string[] args)
+		{
+			Utilities Function = new Utilities();
+			Console.Title = "Login";
+			Console.BackgroundColor = ConsoleColor.White;
+			Console.ForegroundColor = ConsoleColor.Black;
+			Console.Clear();
+			int op=0;
+			
+			CreateDataForDefault();
+			
+			
+			Console.ReadKey(true);
+			Function.Login();
+			Console.Clear();
+			
+			
+			
+			
+			while(true){
+				Console.Title= "Menú";
+				Console.Clear();
+				Console.WriteLine("Bienvenido\n");
+				Console.WriteLine("¿Qué desea realizar?");
+				Console.WriteLine("1. Buscar Libro");
+				Console.WriteLine("2. Registrar prestamo");
+				Console.WriteLine("3. Consultar prestamos");
+				Console.WriteLine("4. Salir\n");
+				Console.Write("Opción: ");
+				
+				try{
+					op = Convert.ToInt32(Console.ReadLine());
+					
+				}catch(Exception e){
+					Console.WriteLine(e.Message);
+				}
+				
+				switch(op){
+						
+					case 1:
+						Console.Clear();
+						Console.Title = "Biblioteca";
+						string ruta= @"C:\dataProject\Libros\Faker.txt";
+						string lectura;
+						StreamReader flujodelectura=null;
+			
+						try{
+				
+						flujodelectura = new StreamReader(ruta);
+							while (!flujodelectura.EndOfStream){
+								lectura = flujodelectura.ReadLine();
+								Console.WriteLine(lectura);
+								
+							}
+						} catch (Exception e){
+							
+							Console.WriteLine("Error: " + e.Message);
+						}finally{
+							if(flujodelectura!=null){
+								flujodelectura.Close();
+							}
+							
+							Console.ReadKey();
+							
+						}
+		
+						break;
+						
+						
+						
+					case 2:
+						
+						Console.WriteLine("¿Qué libro desea elegir?");
+						int op2 = 0;
+						try{
+						op2 = Convert.ToInt32(Console.ReadLine());
+						}catch(Exception e){
+							Console.WriteLine(e.Message);
+						}
+						break;
+						
+					case 4:
+						DateTime now = DateTime.Now;
+						Console.WriteLine(now);
+						Console.ReadKey(true);
+						Environment.Exit(0);
+						break;
+			
+			
+				}
+			}
+		} // End of Main()
+		
+		static void CreateDataForDefault(){
+			Program program = new Program();
+			Utilities Function = new Utilities();
+			string pathUsers = program.PathDataProject + @"\Users";
+			string pathUsersLog = pathUsers + @"\Users.txt";
+			string pathRegistry = program.PathDataProject + @"\Registry";
+			string pathBooks = pathRegistry + @"\Books";
+			
+			if (!Directory.Exists(pathUsers)) {
+				Function.CreateDirectory(pathUsers);
+				
+				if (!File.Exists(pathUsersLog))
+		        {
+					
+		            // Create a file to write to.
+		            using (StreamWriter sw = File.CreateText(pathUsersLog))
+		            {
+		                sw.WriteLine("admin");
+		                sw.WriteLine("admin");
+		            }
+		        }
+		
+		        // Open the file to read from.
+		        using (StreamReader sr = File.OpenText(pathUsersLog))
+		        {
+		            string s;
+		            while ((s = sr.ReadLine()) != null)
+		            {
+		                Console.WriteLine(s);
+		            }
+		        }
+			}else{
+				Console.WriteLine("The path already exists :)");
+				using (StreamReader sr = File.OpenText(pathUsersLog))
+		        {
+		            string s;
+		            while ((s = sr.ReadLine()) != null)
+		            {
+		                Console.WriteLine(s);
+		            }
+		        }
+				Console.ReadKey(true);
+			}
+			
+			if (!Directory.Exists(pathRegistry)) {
+				Function.CreateDirectory(pathRegistry);
+				Function.CreateDirectory(pathBooks);
+				
+				CreateBook(pathBooks, "1. La Biblia de CSharp - Anaya");
+		
+		        // Open the file to read from.
+		        using (StreamReader sr = File.OpenText(pathUsersLog))
+		        {
+		            string s;
+		            while ((s = sr.ReadLine()) != null)
+		            {
+		                Console.WriteLine(s);
+		            }
+		        }
+			}else{
+				
+			}
+			
+		} // End of CreateDataForDefault()
+		
+		public static void CreateBook( string pathBook, string name){
+			string pathComplete = pathBook + name + @".txt";
+			if (!File.Exists(pathComplete))
+			{
+				// Create a file to write to.
+				using (StreamWriter sw = File.CreateText(pathComplete))
+				{
+					sw.WriteLine(name);
+				}
+			}
+		}// End of CreateBook()
+		
+	}// End of Program
+}
---
+++ Gestor_De_Libros/Utilities.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+
+namespace Gestor_De_Libros
+{
+	/// <summary>
+	/// Description of Utilities.
+	/// </summary>
+	public class Utilities
+	{
+		Program program = new Program();
+		string pathDataProject = null;
+		
+		public Utilities()
+		{
+			pathDataProject = program.PathDataProject;
+		}
+		
+		public void Login(){
+			string username = null,password = null, userIn = null, passIn = null;
+			string pathLogin = pathDataProject + @"\Users\Users.txt";
+			while(true){
+				Console.Clear();
+				
+				using (StreamReader ReadLogin = new StreamReader(File.Open(pathLogin, FileMode.Open))) {
+				username = ReadLogin.ReadLine();
+				password = ReadLogin.ReadLine();
+				ReadLogin.Close();
+				}
+				Console.Write("Nombre: ");
+				userIn = Console.ReadLine();
+				Console.Write("Contraseña: ");
+				passIn = Console.ReadLine();
+				if (userIn != username){
+					Console.WriteLine("Nombre de usuario incorrecto");
+				} 
+				if (passIn != password){
+					Console.WriteLine("Contraseña incorrecta");
+				}
+				if (username == userIn && password == passIn) {
+					Console.Write("LOGIN EXITOSO"); 
+					Console.ReadKey();
+					break;
+				}
+				Console.Write("LOGIN FALLIDO"); 
+				Console.ReadKey(true);
+			}	
+		}
+		public void Register(){
+			
+		}
+		public void CreateDirectory(string path){
+			try 
+	        {
+	            // Determine whether the directory exists.
+	            if (Directory.Exists(path)) 
+	            {
+	                return;
+	            }
+	
+	            // Try to create the directory.
+	            DirectoryInfo di = Directory.CreateDirectory(path);
+	
+
+	        } 
+	        catch (Exception e) 
+	        {
+	            Console.WriteLine("The process failed: {0}", e.ToString());
+	        } 
+		}
+		public void DeleteDirectory(string path){
+			Directory.Delete(path, true);
+		}
+	}
+}
