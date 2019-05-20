1c15e1de5a3b0a17633a022900f28274c36a1e68
Elvis Gastelum <elvisgastelum@outlook.com>
2019-05-20 11:40:04 -0700

    Se agregaron logs y arreglaron paths

--- Gestor_De_Libros/Program.cs
+++ Gestor_De_Libros/Program.cs
@@ -1,23 +1,43 @@
 ﻿using System;
 using System.IO;
-using System.Security.AccessControl;
+
 
 namespace Gestor_De_Libros
 {
 	
 	class Program
 	{
-		readonly public string pathDataProject = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\dataProject";
+		readonly string pathDataProject = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\dataProject";
+		string pathUsers = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\dataProject\Users";
+		string pathBooks = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\dataProject\Registry\Books";
+		string pathlogs = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\dataProject\Logs";
 
+		public string Pathlogs {
+			get {
+				return pathlogs;
+			}
+		}
 		public string PathDataProject {
 			get {
 				return pathDataProject;
 			}
 			
 		}
+		public string PathUsers {
+			get {
+				return pathUsers;
+			}
+		}
+		public string PathBooks {
+			get {
+				return pathBooks;
+			}
+		}
+
 		public static void Main(string[] args)
 		{
 			Utilities Function = new Utilities();
+			Program program = new Program();
 			Console.Title = "Login";
 			Console.BackgroundColor = ConsoleColor.White;
 			Console.ForegroundColor = ConsoleColor.Black;
@@ -72,22 +92,23 @@ namespace Gestor_De_Libros
 						
 							if(BookSelected == 1){
 							Console.Clear();
-							string Ruta= File.ReadAllText(@"C:\Users\Yisus\Desktop\gestor-de-libros-master\Gestor_De_Libros\archives\Libros\Caperucita Roja.txt");
-							Console.WriteLine(Ruta);
+							string Ruta= Function.ReadFile(string.Format(@"{0}\Caperucita Roja.txt", program.PathBooks));
+							detallesDelLibro(Ruta);
 							Console.ReadKey();
 							}
 						
 						if(BookSelected == 2){
 							Console.Clear();
-							string Ruta= File.ReadAllText(@"C:\Users\Yisus\Desktop\gestor-de-libros-master\Gestor_De_Libros\archives\Libros\La Biblia de CSharp - Anaya.txt");
-							Console.WriteLine(Ruta);
+							string Ruta = Function.ReadFile(string.Format(@"{0}\La Biblia de CSharp - Anaya.txt", program.PathBooks));
+							
+							detallesDelLibro(Ruta);
 							Console.ReadKey();
 							}
 						
 							if(BookSelected == 3){
 							Console.Clear();
-							string Ruta = File.ReadAllText(@"C:\Users\Yisus\Desktop\gestor-de-libros-master\Gestor_De_Libros\archives\Libros\Libros\Los 3 Cochinitos.txt");
-							Console.WriteLine(Ruta);
+							string Ruta = Function.ReadFile(string.Format(@"{0}\Los Tres Cochinitos.txt", program.PathBooks));
+							detallesDelLibro(Ruta);
 							Console.ReadKey();
 						}else{
 							continue;
@@ -112,9 +133,15 @@ namespace Gestor_De_Libros
 						DateTime now = DateTime.Now;
 						Console.WriteLine(now);
 						Console.ReadKey(true);
+						string logDate = now.ToString();
+			
+						Function.WriteLineInFile("Salida >> Log: " + logDate, program.Pathlogs + @"\logs.txt");
 						Environment.Exit(0);
 						break;
-			
+					default:
+						Console.WriteLine("Opcion no valida! :(");
+						Console.ReadKey();
+						break;
 			
 				}
 			}
@@ -125,45 +152,76 @@ namespace Gestor_De_Libros
 			Utilities Function = new Utilities();
 			string pathUsers = program.PathDataProject + @"\Users";
 			string pathUsersLog = pathUsers + @"\Users.txt";
-			string pathRegistry = program.PathDataProject + @"\Registry";
-			string pathBooks = pathRegistry + @"\Books";
+			string pathBooks = program.PathDataProject + @"\Registry\Books";
+			DateTime now = DateTime.Now;
+			string logDate = now.ToString();
 			
+			
+			
+			if (!Directory.Exists(program.Pathlogs)) {
+				Function.CreateDirectory(program.Pathlogs);
+			}
+			Function.WriteLineInFile("Entrada >> Log: " + logDate, program.Pathlogs + @"\logs.txt");
 			if (!Directory.Exists(pathUsers)) {
 				Function.CreateDirectory(pathUsers);
 				
 				if (!File.Exists(pathUsersLog))
 		        {
 					
-					Function.WriteInFile("admin", pathUsersLog);
-		            Function.WriteInFile("admin", pathUsersLog);
+					Function.WriteLineInFile("admin", pathUsersLog);
+		            Function.WriteLineInFile("admin", pathUsersLog);
 		        }
 		
 		        
 			}else{
 				if (!File.Exists(pathUsersLog))
 		        {
-					Function.WriteInFile("admin", pathUsersLog);
-		            Function.WriteInFile("admin", pathUsersLog);
+					Function.WriteLineInFile("admin", pathUsersLog);
+		            Function.WriteLineInFile("admin", pathUsersLog);
 		        }
 			}
 			
-			if (!Directory.Exists(pathRegistry)) {
-				Function.CreateDirectory(program.PathDataProject +  @"\Registry\Books");
+			if (!Directory.Exists(program.PathDataProject + @"\Registry")) {
+				string pathBooksIn = program.PathBooks +  @"\";
+				Function.CreateDirectory(program.PathBooks);
 				
-				CreateBook(program.PathDataProject +  @"\Registry\Books\", "1. La Biblia de CSharp - Anaya");
+				CreateBook(pathBooksIn, "La Biblia de CSharp - Anaya", "Jason Jeff Brian Beres", "2003", "978-844-151-484-3", "861");
+				CreateBook(pathBooksIn, "Caperucita Roja", "Wilhelm i Jacob Grimm", "1697, 1812", "978-84-7864-851-1", "24");
+				CreateBook(pathBooksIn, "Los Tres Cochinitos", "Roberto Bravo Meritxell Marti", "1900", "978-84-7864-765-1", "24");
+				
 			}
 			
 		} // End of CreateDataForDefault()
 		
-		public static void CreateBook( string pathBook, string name){
+		public static void CreateBook( string pathBook, string name, string autor, string AImpresion, string isbn, string NumPag){
 			Utilities Function = new Utilities();
 			string pathComplete = pathBook + name + @".txt";
 			if (!File.Exists(pathComplete))
 			{
 				// Create a file to write to.
-				Function.WriteInFile(name, pathComplete);
+				Function.WriteLineInFile(name, pathComplete);
+				Function.WriteLineInFile(autor, pathComplete);
+				Function.WriteLineInFile(AImpresion, pathComplete);
+				Function.WriteLineInFile(isbn, pathComplete);
+				Function.WriteInFile(NumPag, pathComplete);
 			}
 		}// End of CreateBook()
+		
+		static void detallesDelLibro(string Ruta){
+			string[] _path = Ruta.Split('\n');
+			int i = 0;
+			string[] option = {
+				"Nombre del libro: ",
+				"Autor: ",
+				"Año de impresión: ",
+				"ISBN: ",
+				"No. Páginas: "
+			};
+			foreach (string line in _path) {
+				Console.WriteLine(option[i] + line);
+				i++;
+			}
+		}// End of detallesDelLibro()
 		
 	}// End of Program
 }
